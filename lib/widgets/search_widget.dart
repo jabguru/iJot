5b10f849926c880c51b3f823d0a71053a0695567
Julius Alibrown <brownjulius980@gmail.com>
2023-07-12 15:12:56 +0100

    fixed the search bar content padding issue

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:animated_search_bar/animated_search_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ijot/constants/colors.dart';
 import 'package:ijot/constants/constants.dart';
@@ -7,6 +10,8 @@ import 'package:ijot/constants/constants.dart';
 class SearchWidget extends StatelessWidget {
   const SearchWidget({required this.onChanged, super.key});
   final ValueChanged<String> onChanged;
+
+  bool get _isMobile => !kIsWeb && (Platform.isAndroid || Platform.isIOS);
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +26,14 @@ class SearchWidget extends StatelessWidget {
           onChanged: onChanged,
           searchDecoration: kTextFieldDecoration.copyWith(
             hintText: 'search'.tr(),
-            contentPadding: const EdgeInsets.only(
-              top: 0.0,
-              bottom: 20.0,
-              left: 20.0,
-              right: 20.0,
-            ),
+            contentPadding: !_isMobile
+                ? const EdgeInsets.only(
+                    top: 0.0,
+                    bottom: 20.0,
+                    left: 20.0,
+                    right: 20.0,
+                  )
+                : null,
           ),
         ),
       ),
